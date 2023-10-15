28c735949792b2cfd6e322a51c696677faf2acb9
Muhdi hell Code <115169961+muhdi15@users.noreply.github.com>
2023-10-15 21:59:50 +0800

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,17 @@
+
+package pkg100daycode;
+
+
+public class day7 {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 100; i++) {
+            System.out.println("");
+            for (int j = 0; j < i; j++) {
+                System.out.print(j);
+                
+            }
+            
+        }
+    }
+}
