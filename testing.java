ecc1a7a5b73ea0e1516c0ca4c9ee68e593332ee2
Muhdi hell Code <115169961+muhdi15@users.noreply.github.com>
2023-10-28 21:12:33 +0700

    Add files via upload

---
+++ testing.java
@@ -0,0 +1,16 @@
+package Praktikum3.main;
+
+import Praktikum3.*;
+
+public class testing {
+
+    public static void main(String[] args) {
+        String[] obj = {"Bag", "Bike", "Book", "Calculator", "Car", "Cat", "Circle", "Dog", "Door", "Fan", "FlashLight", "Kotak", "Lamp", "Mathches", "Pen", "River", "Stove", "Switch", "Traffic", "Triangle", "Weather"};
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(obj[9] + " " + obj[9].toLowerCase() + i + "= new " + obj[9] + "();");
+
+        }
+    }
+
+}
